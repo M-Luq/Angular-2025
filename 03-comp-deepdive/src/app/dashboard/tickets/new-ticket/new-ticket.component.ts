9bf08d4b12dc0ced847742d9d38cf71f7b3f0cc8
M-Luq <mohamedluqman766@gmail.com>
2025-11-13 17:48:36 +0530

    Day 16

--- 03-comp-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-comp-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent implements OnInit,AfterViewInit {
+  enteredTitle = '';
+  enteredText = '';
   @ViewChild('form') private form?:ElementRef<HTMLFormElement>;
     // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+    //@Output() add = new EventEmitter<{title:string;text:string}>();
+
+    add = output<{title:string;text:string}>();
 
   ngOnInit(){
     console.log("on init called");
@@ -23,9 +28,12 @@ export class NewTicketComponent implements OnInit,AfterViewInit {
     console.log(this.form?.nativeElement);
    }
 
-  onSubmit(title: string,request: string) {
-    console.log(title);
-    console.log(title,request);// to log as object
-    this.form?.nativeElement.reset();// to reset the form after submission
+  onSubmit() {
+    //console.log(title);
+    //console.log(title,request);// to log as object
+    this.add.emit({title:this.enteredTitle,text:this.enteredText})
+    //this.form?.nativeElement.reset();// to reset the form after submission
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
