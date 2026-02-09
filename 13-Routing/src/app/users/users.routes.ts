c24808c5b94b16e222333793d90efdaa0bc2e537
M-Luq <mohamedluqman766@gmail.com>
2026-02-09 16:52:58 +0530

    Day - 31 - Routes end

--- 13-Routing/src/app/users/users.routes.ts
+++ 13-Routing/src/app/users/users.routes.ts
@@ -13,6 +13,7 @@ export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
+    runGuardsAndResolvers:'always',
     resolve: {
       userTasks: resolveUserTasks,
     },
